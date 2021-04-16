c1a70fa42a34862d75b5affaff0ba24159a958ec
Pedro Miguel Sousa Bernardo <pedro.m.s.bernardo@tecnico.ulisboa.pt>
2021-04-16 17:44:27 +0200

    first commit

---
+++ Assets/DrawLines.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class DrawLines : MonoBehaviour
+{
+    [SerializeField] public Camera camera;
+    private Texture2D m_Texture;
+    private Color[] m_Colors;
+    RaycastHit2D hit;
+    SpriteRenderer spriteRend;
+    Color zeroAlpha = Color.clear;
+    public int erSize;
+    public Vector2Int lastPos;
+    public bool Drawing = false;
+    void Start ()
+    {
+        spriteRend = gameObject.GetComponent<SpriteRenderer>();
+        var tex = spriteRend.sprite.texture;
+        m_Texture = new Texture2D(tex.width, tex.height, TextureFormat.ARGB32, false);
+        m_Texture.filterMode = FilterMode.Bilinear;
+        m_Texture.wrapMode = TextureWrapMode.Clamp;
+        m_Colors = tex.GetPixels();
+        m_Texture.SetPixels(m_Colors);
+        m_Texture.Apply();
+        spriteRend.sprite = Sprite.Create(m_Texture, spriteRend.sprite.rect, new Vector2(0.5f, 0.5f));
+	}
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var mousepos = camera.ScreenToWorldPoint(Input.mousePosition);
+            mousepos.z = 3;
+            Debug.Log("raw " + Input.mousePosition);
+            Debug.Log("cam " + camera.ScreenToWorldPoint(new Vector3(mousepos.x, mousepos.y, camera.nearClipPlane)));
+            hit = Physics2D.Raycast(new Vector3(mousepos.x, mousepos.y, camera.nearClipPlane), Vector2.zero);
+            
+            if (hit.collider != null)
+            {
+                UpdateTexture();
+                Drawing = true;
+            }
+        }
+        else
+            Drawing = false;
+    }
+
+    public void UpdateTexture()
+    {
+        int w = m_Texture.width;
+        int h = m_Texture.height;
+        var mousePos = hit.point - (Vector2)hit.collider.bounds.min;
+        Debug.Log("mouse pos: " + mousePos.x);
+        Debug.Log("mouse pos: " + mousePos.y);
+        mousePos.x *= w / hit.collider.bounds.size.x;
+        mousePos.y *= h / hit.collider.bounds.size.y;
+        Vector2Int p = new Vector2Int((int)mousePos.x, (int)mousePos.y);
+        Vector2Int start = new Vector2Int();
+        Vector2Int end = new Vector2Int();
+        if(!Drawing)
+            lastPos = p;
+        start.x = Mathf.Clamp(Mathf.Min(p.x, lastPos.x) - erSize, 0, w);
+        start.y = Mathf.Clamp(Mathf.Min(p.y, lastPos.y) - erSize, 0, h);
+        end.x = Mathf.Clamp(Mathf.Max(p.x, lastPos.x) + erSize, 0, w);
+        end.y = Mathf.Clamp(Mathf.Max(p.y, lastPos.y) + erSize, 0, h);
+        Vector2 dir = p - lastPos;
+        for (int x = start.x; x < end.x; x++)
+        {
+            for (int y = start.y; y < end.y; y++)
+            {
+                Vector2 pixel = new Vector2(x, y);
+                Vector2 linePos = p;
+                if (Drawing)
+                {
+                    float d = Vector2.Dot(pixel - lastPos, dir) / dir.sqrMagnitude;
+                    d = Mathf.Clamp01(d);
+                    linePos = Vector2.Lerp(lastPos, p, d);
+                }
+                if ((pixel - linePos).sqrMagnitude <= erSize * erSize)
+                {
+                    m_Colors[x + y * w] = zeroAlpha;
+                }
+            }
+        }
+        lastPos = p;
+        m_Texture.SetPixels(m_Colors);
+        m_Texture.Apply();
+        spriteRend.sprite = Sprite.Create(m_Texture, spriteRend.sprite.rect, new Vector2(0.5f, 0.5f));
+    }
+}
---
+++ Assets/DrawScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawScript : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] GameObject brush;
+    [SerializeField] Texture grass;
+
+    Texture2D texture;
+
+    
+
+    LineRenderer currentLineRenderer;
+    Vector2 lastPos;
+    bool drawing;
+    // Start is called before the first frame update
+
+    public void Draw(){
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            // CreateBrush();   
+            drawing = true;
+        }
+        else if(Input.GetKey(KeyCode.Mouse0)){
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            // Debug.Log(grass.material.mainTexture);
+
+            // Color originalColour = renderer.material.color;
+            // renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 0);
+            // if(mousePos != lastPos){
+            //     AddAPoint(mousePos);
+            //     lastPos = mousePos;
+            // }
+        }
+        else{
+            if(drawing){
+                drawing = !drawing;
+
+                
+            }
+        }
+    }
+
+    void CreateBrush(){
+        GameObject brushInstance = Instantiate(brush);
+        currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
+
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        currentLineRenderer.SetPosition(0, mousePos);
+        currentLineRenderer.SetPosition(1, mousePos);
+
+    }
+
+    void AddAPoint(Vector2 pointPos){
+        currentLineRenderer.positionCount++;
+        int positionIndex = currentLineRenderer.positionCount - 1;
+        currentLineRenderer.SetPosition(positionIndex, pointPos);
+    }
+
+    void Start()
+    {
+        drawing = false;
+        // texture = new Texture2D(512, 512);
+        // grass.material.mainTexture = texture;
+        // texture.Apply();
+
+        Texture2D texture = new Texture2D(128, 128);
+        // Sprite sprite = Sprite.Create(texture, new Rect(0, 0, 128, 128), Vector2.zero);
+        // grass.GetComponent<MeshRenderer>().sprite = sprite;
+     
+        // for (int y = 0; y < texture.height; y++)
+        // {
+        //     for (int x = 0; x < texture.width; x++) //Goes through each pixel
+        //     {
+        //         Color pixelColour;
+        //        if (Random.Range(0, 2) == 1) //50/50 chance it will be black or white
+        //         {
+        //             pixelColour = new Color(0, 0, 0, 1);
+        //         }
+        //         else
+        //         {
+        //             pixelColour = new Color(1, 1, 1, 1);
+        //         }
+        //         texture.SetPixel(x, y, pixelColour);
+        //     }
+        // }
+        // texture.Apply();
+
+        //    grass.GetComponent<Renderer>().material.mainTexture = texture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Draw();
+    }
+}
